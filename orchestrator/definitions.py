6129ff4f400adb2ac67219678c81b0350db9fd23
lauramarion <noir.colore.inc@gmail.com>
2025-11-08 15:34:25 +0100

    add back nocodb in definitions.py

--- orchestrator/definitions.py
+++ orchestrator/definitions.py
@@ -1,5 +1,5 @@
 # definitions.py
-
+import os
 from dagster import Definitions, load_assets_from_modules 
 import assets
 
@@ -11,9 +11,9 @@ all_assets = load_assets_from_modules([assets])
 # 2. DEFINE RESOURCE CONFIGURATION
 # -----------------
 
-#nocodb_resource_configured = DbConnectionResource(
-#    host="nocodb",
-#)
+nocodb_resource_configured = DbConnectionResource(
+    host="nocodb",
+)
 
 # -----------------
 # 3. DEFINE THE FINAL PLAN
@@ -21,7 +21,7 @@ all_assets = load_assets_from_modules([assets])
 defs = Definitions(
     assets=all_assets,
     
-#    resources={
-#        "nocodb_db": nocodb_resource_configured,
-#    },
+    resources={
+        "nocodb_db": nocodb_resource_configured,
+    },
 )
