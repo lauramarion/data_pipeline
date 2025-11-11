751e635456347489ff33842be831902dfa5da38b
lauramarion <noir.colore.inc@gmail.com>
2025-11-11 14:12:59 +0100

    fix attempt n5464

--- orchestrator/definitions.py
+++ orchestrator/definitions.py
@@ -3,15 +3,21 @@ from dagster import Definitions, load_assets_from_modules
 import assets
 from resources import DbConnectionResource
 
+DB_PASSWORD_SECRET = "299centblues"
+
 all_assets = load_assets_from_modules([assets])
 
 # -----------------
 # 2. DEFINE RESOURCE CONFIGURATION
 # -----------------
 
-#nocodb_resource_configured = DbConnectionResource.configured({
-#"password": os.environ.get("NOCODB_DB_PASSWORD"),
-#})
+nocodb_resource_configured = DbConnectionResource.configured({
+"database": "nocodb",
+    "user": "postgres",
+    "password": DB_PASSWORD_SECRET, 
+    "host": "db", 
+    "port": 5432
+})
 
 # -----------------
 # 3. DEFINE THE FINAL PLAN
